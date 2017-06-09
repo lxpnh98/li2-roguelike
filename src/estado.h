2f533579161c518d1578e043c4e7acca49975b18
Alexandre Pinho <lxpnh98@gmail.com>
2017-06-09 16:26:53 +0100

    Comentar ficheiros estado.c e estado.h.

--- src/estado.h
+++ src/estado.h
@@ -14,39 +14,54 @@ Definição do estado, de contantes relevantes e das funções que o inicializam
 /** \brief O nº máximo de obstáculos */
 #define MAX_OBSTACULOS		100
 
+/** \brief O nº máximo de caracteres em buffer */
 #define MAX_BUFFER              10240
 
+/** \brief Tamanho do mapa */
 #define TAM                     9
 
+/** \brief Escala das imagens das casas */
 #define ESCALA                  40
 
+/** \brief O nº de tipos de inimigos */
 #define NUM_TIPO_INIMIGOS       3
 
+/** \brief O nº inicial de vidas do jogador */
 #define INIT_VIDAS              5
 
+/** \brief A constante que serve para indicar se a funcão rand_pos() testa pela saída */
 #define TESTAR_SAIDA            1
 
-/**
-\brief Estrutura que armazena uma posição
-*/
+/** \brief Estrutura que armazena uma posição */
 typedef struct posicao {
+        /** \brief A coordenada X da posição */
 	int x;
+        /** \brief A coordenada Y da posição */
 	int y;
 } POSICAO;
 
+/** \brief Estrutura que representa um inimigo */
 typedef struct inimigo {
+    /** \brief O tipo do inimigo */
     enum {GUERREIRO, CORREDOR, CAVALEIRO} tipo;
+    /** \brief A posição do inimigo */
     POSICAO pos;
 } INIMIGO;
 
+/** \brief Estrutura que representa o jogador */
 typedef struct jogador {
-	POSICAO pos;
-	int score;
-	int vidas;
-	enum {NORMAL     = 'n', 
-              ATAQUE     = 'a',
-              MUDAR_MODO = 'm'} modo;
+    /** \brief A posição do jogador */
+    POSICAO pos;
+    /** \brief A pontuação do jogador */
+    int score;
+    /** \brief O nº de vidas do jogador */
+    int vidas;
+    /** \brief O modo de jogo do estado */
+    enum {NORMAL     = 'n', 
+          ATAQUE     = 'a',
+          MUDAR_MODO = 'm'} modo; // Mudar para estado.
 } JOGADOR;
+
 /**
 \brief Estrutura que armazena o estado do jogo
 */
